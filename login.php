069c14e015f2ff5279bbc334f8aeb7ff2c8febb4
Unique Bhattarai <uniqgaming26@gmail.com>
2020-05-08 11:52:59 +0545

    just frontend
    nothig changes made

--- login.php
+++ login.php
@@ -2,4 +2,4 @@
 include("include/header.include.php");
 include("include/banner.include.php");
 include("include/login/login.include.php");
-include("include/footer.include.php");
+    include("include/footer.include.php");
