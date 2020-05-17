7c83fc3a85aae5d7e481caceffc05ce62e204818
sabdika <47735309+sabdika@users.noreply.github.com>
2020-05-17 22:23:36 +0545

    connection

---
+++ include/admin_area/includes/sidebar.php
@@ -0,0 +1,221 @@
+<?php 
+    
+    if(!isset($_SESSION['admin_email'])){
+        
+        echo "<script>window.open('login.php','_self')</script>";
+        
+    }else{
+
+?>
+   
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background:#008060;"><!-- navbar navbar-inverse navbar-fixed-top begin -->
+    <div class="navbar-header"><!-- navbar-header begin -->
+        
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><!-- navbar-toggle begin -->
+            
+            <span class="sr-only">Toggle Navigation</span>
+            
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            
+        </button><!-- navbar-toggle finish -->
+        
+        <a href="index.php?dashboard" class="navbar-brand" style="color: white;">Admin Area</a>
+        
+    </div><!-- navbar-header finish -->
+    
+    <ul class="nav navbar-right top-nav"><!-- nav navbar-right top-nav begin -->
+        
+        <li class="dropdown"><!-- dropdown begin -->
+            
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color: black;"><!-- dropdown-toggle begin -->
+                
+                <i class="fa fa-user"></i> <?php echo $admin_name;  ?> <b class="caret"></b>
+                
+            </a><!-- dropdown-toggle finish -->
+            
+            <ul class="dropdown-menu"><!-- dropdown-menu begin -->
+                <li><!-- li begin -->
+                    <a href="index.php?user_profile=<?php echo $admin_id; ?>"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-user"></i> Profile
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_products"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-envelope"></i> Products
+                        
+                        <span class="badge"><?php echo $count_products; ?></span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_customers"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-users"></i> Customeres
+                        
+                        <span class="badge"><?php echo $count_customers; ?></span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li><!-- li begin -->
+                    <a href="index.php?view_cats"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-gear"></i> Brands
+                        
+                        <span class="badge"><?php echo $count_p_categories; ?></span>
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+                <li class="divider"></li>
+                
+                <li><!-- li begin -->
+                    <a href="logout.php"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-power-off"></i> Log Out
+                        
+                    </a><!-- a href finish -->
+                </li><!-- li finish -->
+                
+            </ul><!-- dropdown-menu finish -->
+            
+        </li><!-- dropdown finish -->
+        
+    </ul><!-- nav navbar-right top-nav finish -->
+    
+    <div class="collapse navbar-collapse navbar-ex1-collapse" ><!-- collapse navbar-collapse navbar-ex1-collapse begin -->
+        <ul class="nav navbar-nav side-nav" style="background:#008060;"><!-- nav navbar-nav side-nav begin -->
+            <li ><!-- li begin -->
+                <a href="index.php?dashboard" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-dashboard"></i> Dashboard
+                        
+                </a><!-- a href finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#products" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-tag"></i> Products
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="products" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_product" style="color: white;"> Insert Product </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_products" style="color: white;"> View Products </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#p_cat" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-edit"></i> Brands 
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="p_cat" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_p_cat" style="color: white;"> Insert Brand </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_p_cats" style="color: white;"> View Brands </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#cat" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-book"></i> Categories
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="cat" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_cat" style="color: white;"> Insert Category </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_cats" style="color: white;"> View Categories </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#slides" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-gear"></i> Slides
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="slides" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_slide" style="color: white;"> Insert Slide </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_slides" style="color: white;"> View Slides </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="index.php?view_customers" style="color: yellow;"><!-- a href begin -->
+                    <i class="fa fa-fw fa-users"></i> View Customers
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+            
+            <li><!-- li begin -->
+                <a href="#" data-toggle="collapse" data-target="#users" style="color: yellow;"><!-- a href begin -->
+                        
+                        <i class="fa fa-fw fa-users"></i> Users
+                        <i class="fa fa-fw fa-caret-down"></i>
+                        
+                </a><!-- a href finish -->
+                
+                <ul id="users" class="collapse"><!-- collapse begin -->
+                    <li><!-- li begin -->
+                        <a href="index.php?insert_user" style="color: white;"> Insert User </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?view_users" style="color: white;"> View Users </a>
+                    </li><!-- li finish -->
+                    <li><!-- li begin -->
+                        <a href="index.php?user_profile=<?php echo $admin_id; ?>"style="color: white;"> Edit User Profile </a>
+                    </li><!-- li finish -->
+                </ul><!-- collapse finish -->
+                
+            </li><!-- li finish -->
+            
+            <li><!-- li begin -->
+                <a href="logout.php" style="color: yellow;" ><!-- a href begin -->
+                    <i class="fa fa-fw fa-power-off"></i>Log Out
+                </a><!-- a href finish -->
+            </li><!-- li finish -->
+            
+        </ul><!-- nav navbar-nav side-nav finish -->
+    </div><!-- collapse navbar-collapse navbar-ex1-collapse finish -->
+    
+</nav><!-- navbar navbar-inverse navbar-fixed-top finish -->
+
+
+<?php } ?>
