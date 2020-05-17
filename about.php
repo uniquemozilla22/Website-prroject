4226d447bfb306d110ceddd32f928d0e43ade914
sabdika <47735309+sabdika@users.noreply.github.com>
2020-05-17 15:40:38 +0545

    Update about.php
    including Connection.php for oracle database connection

--- about.php
+++ about.php
@@ -5,3 +5,4 @@ include("include/banner.include.php");
 include("include/about/description.include.php");
 include("include/about/team.include.php");
 include("include/footer.include.php");
+include("include/connection.php");
