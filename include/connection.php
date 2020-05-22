9a69c5af3d1a0c0a8d548405a2124c6ab60aabcd
sabdika <47735309+sabdika@users.noreply.github.com>
2020-05-22 19:06:11 +0545

    connection

--- include/connection.php
+++ include/connection.php
@@ -1,8 +1,13 @@
-<?php $conn = oci_connect('projectmanagement', 'projectmanagement', '//localhost/xe'); 
+<?php 
+/*$conn = oci_connect('projectmanagement', 'projectmanagement', '//localhost/xe'); 
 if (!$conn) {
    $m = oci_error();
    echo $m['message'], "\n";
    exit; } else {
    print "Connected to Oracle!"; } 
-    oci_close($conn); 
+    oci_close($conn); */
+    ?>
+
+    <?php
+    $connection=mysqli_connect("localhost","root","","ecom_store");
     ?>
