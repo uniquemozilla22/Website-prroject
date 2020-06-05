af9cc5868aa7a3b776966bb449b42ec266de34e5
Unique Bhattarai <uniqgaming26@gmail.com>
2020-06-05 11:04:58 +0545

    product page done but image need adjusting

--- singleproduct.php
+++ singleproduct.php
@@ -1,8 +1,8 @@
 <?php
+session_start();
 include("include/connection.php");
 include("include/header.include.php");
 include("include/banner.include.php");
 include("include/products/singleproduct.include.php");
-include("include/products/sidebar.include.php");
 include("include/footer.include.php");
 ?>
