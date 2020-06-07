d7cd79cd6d6d3df544b0f50aba356aee7c9173ef
sabdika <47735309+sabdika@users.noreply.github.com>
2020-06-07 15:13:19 +0545

    Trader_page done finally
    Trader_page done finally

--- include/traders/traderlist.include.php
+++ include/traders/traderlist.include.php
@@ -1,4 +1,4 @@
-<div class="page-shop-sidebar left--sidebar bg--white section-padding--lg">
+<div class="page-shop-sidebar left--sidebar bg--white section-padding--lg" style='background-color:RGB(201, 190, 185);'>
         	<div class="container">			
         			<div class="col-lg-9 col-12 order-1 order-lg-2">
         			
@@ -32,26 +32,26 @@
 						
 								echo "
 						
-        				<div class='tab__container'>
-	        				<div class='shop-grid tab-pane fade show active' id='nav-list' role='tabpanel'>
-	        					<div class='list__view__wrapper'>
-	        						<!-- Start Single Product -->
-	        						<div class='list__view'>
-	        							<div class='thumb'>
-	        								<img src='trader_area/trader_images/$userimage' alt='$userimage' style='margin:20px;'>
-	        								
-	        							</div>
-	        							<div class='content'>
-	        								<h2 style='color:RGB(237, 123, 78)'>$username</h2>
-											<h5>$useremail</h5>
-											<h5>$usercontact</h5>
-	        								
-	        								<p>$userdesc</p>
-	        								
-
-	        							</div>
-	        						</div>
-	        						<!-- End Single Product -->
+								<div class='tab__container'>
+								<div class='shop-grid tab-pane fade show active' id='nav-list' role='tabpanel'>
+									<div class='list__view__wrapper'>
+										<!-- Start Single Product -->
+										<div class='list__view'>
+											<div class='thumb'>
+												<img src='trader_area/trader_images/$userimage' alt='$userimage' style='margin:20px;'>
+												
+											</div>
+											<div class='content'>
+												<h2 style='color:RGB(237, 123, 78)'>$username</h2>
+												<h5>$useremail</h5>
+												<h5>$usercontact</h5>
+												
+												<p>$userdesc</p>
+												
+	
+											</div>
+										</div>
+										<!-- End Single Product -->
 	        						
 	        					</div>
 	        				</div>
